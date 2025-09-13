506e6a9b626f13a05c0607fbc2a3ec49ca60c145
Krishnan Shankar <krishnans2006@gmail.com>
2025-09-13 06:20:02 -0500

    Add flake-based development environment for Typst

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A Nix-flake-based Typst development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              typst
+              tinymist
+            ];
+          };
+        }
+      );
+    };
+}
